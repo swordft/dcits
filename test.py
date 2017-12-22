8f93295d55aceb453f58a4ac77673ac2b58bc6db
fangtao <swordft@163.com>
2017-12-22 09:32:48 +0800

    201712220932

--- test.py
+++ test.py
@@ -17,19 +17,13 @@ conn.close()
 stat_record = {}
 for i in curr_info:
     for j in last_info: 
-        if (i[2],i[3]) == (j[2],j[3]):
-            stat_record['nochanged'] = stat_record.get('nochanged',[])
-            stat_record['nochanged'].append(i)
-        elif i[2] == j[2] and i[3] != j[3]:
+        if i[2] == j[2] and i[3] != j[3]:
             stat_record['changed'] = stat_record.get('changed',[])
             stat_record['changed'].append(i)
-    if i[2] not in [sn[2] for sn in last_info]:
-        stat_record['new'] = stat_record.get('new',[])
-        stat_record['new'].append(i)
+stat_record['nochanged'] = [ i for i in curr_info if (i[2],i[3]) in [(j[2],j[3]) for j in last_info]]
+#stat_record['changed'] = [ i for i in curr_info if i[2]==j[2] for j in last_info] 
+stat_record['new'] = [i for i in curr_info if i[2] not in [j[2] for j in last_info]]
+stat_record['deleted'] = [ i for i in last_info if i[2] not in [j[2] for j in curr_info]]
 
-for i in last_info:
-    if i[2] not in [sn[2] for sn in curr_info]:
-        stat_record['deleted'] = stat_record.get('deleted',[])
-        stat_record['deleted'].append(i)
 
 print stat_record
